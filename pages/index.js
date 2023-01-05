bb592913e15667b6f9be8dbf871bc787e6d5aba8
denisjunior00 <denis.silva.ju@gmail.com>
2023-01-05 11:50:42 -0400

    teste

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { api, lojaId, urlRequisicao } from "../utils";
 
 export default function Home({destaques}) {  
-  return <div>{destaques[0].vei_id}</div>
+  return <div>{destaques.map(item => { return <div>{item.vei_id}</div>})}</div>
 }
 
 export async function getStaticProps(){
